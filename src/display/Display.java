ffda8b56c557417247a7516dc6b6810b2309ed2f
homosapien97 <christian.antoni.gutowski@gmail.com>
2015-05-16 21:47:59 -0400

    Changed screen dims

--- src/display/Display.java
+++ src/display/Display.java
@@ -3,8 +3,8 @@ package display;
 public class Display {
 	public static final int SPRITE_DIM = 16;
 	
-	public static final int WIDTH = 112; //sprites
-	public static final int HEIGHT = 36;
+	public static final int WIDTH = 96; //sprites
+	public static final int HEIGHT = 54;
 	
 	
 	/*
