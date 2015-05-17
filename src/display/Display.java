ecf8929f745f9ca6e400cc937bfeb704070b263b
Michael <micron1314@gmail.com>
2015-05-16 23:21:52 -0400

    start of graphics

--- src/display/Display.java
+++ src/display/Display.java
@@ -1,13 +1,33 @@
 package display;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Display {
+	//dimensions of sprites in screen pixels
 	public static final int SPRITE_DIM = 16;
 	
-	public static final int WIDTH = 96; //sprites
+	//units are sprites
+	public static final int WIDTH = 96;
 	public static final int HEIGHT = 54;
 	
+	//the output
+	private static JPanel display = new JPanel();
 	
-	/*
-	 * For Michael
-	 */
+	//screen size
+	private static Dimension screenDimensions = new Dimension(WIDTH * SPRITE_DIM, HEIGHT * SPRITE_DIM);
+	public static final Dimension DIMENSIONS = screenDimensions;
+	
+	//initialization
+	static {
+		//set screen size
+		display.setSize(DIMENSIONS);
+		
+		//background color
+		display.setBackground(Color.black);
+	}
+	
+	public static JPanel getDisplay(){
+		return display;
+	}
 }
--- src/run/Main.java
+++ src/run/Main.java
@@ -2,19 +2,33 @@ package run;
 
 import display.Camera;
 import display.TextDisplay;
+import display.Display;
+
 import entity.Player;
+
 import terrain.Stone;
+
 import world.Chunk;
 import world.LoadedChunks;
 
+import javax.imageio.ImageIO;
+
+import javax.swing.JFrame;
+
+import java.awt.Image;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-//	Chunk blank = new Chunk();
+	//example chunk
 	static Chunk testChunk = new Chunk(0,0,true,Stone.get());
+	
+	//player object
 	static Player player = new Player(0,0,testChunk, "0", 256, 64, "Player");
-//	LoadedChunks chunks = new LoadedChunks(testChunk);
-//	Camera camera;
-//	Display display;
-//	TextDisplay debugDisplay;
+	
+	//graphics
+	static JFrame display = new JFrame("Grid Game");
 	
 	public static void step() {
 		stepState();
@@ -22,14 +36,41 @@ public class Main {
 	}
 	
 	private static void stepGFX() {
-		TextDisplay.print();
+		//TextDisplay.print();
+		display.setVisible(true);
 	}
+	
 	private static void stepState() {
 		player.goToAbsolute(-1, -1);
 	}
+	
 	private static void init() {
+		//general init
 		Camera.init(player);
 		LoadedChunks.init(testChunk);
+		
+		//graphics init
+		
+		//content pane
+		display.getContentPane().add(Display.getDisplay());
+		display.pack();
+		
+		//size
+		display.setSize(Display.DIMENSIONS);
+		
+		//icon
+		Image gameicon = null;
+		
+		File icon = new File("resources\\icons\\game_icon.jpeg");
+		
+		try{
+			gameicon = ImageIO.read(icon);
+		}catch(IOException e){}
+		
+		display.setIconImage(gameicon);
+		
+		//general
+		display.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 	public static void main(String[] args) {
