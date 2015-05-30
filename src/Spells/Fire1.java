813d2ed458fb0c79379494cd9f732e0fe86dc0d7
homosapien97 <christian.antoni.gutowski@gmail.com>
2015-05-30 01:37:27 -0400

    Example static spell instance

--- src/Spells/Fire1.java
+++ src/Spells/Fire1.java
@@ -2,9 +2,16 @@ package Spells;
 import magic.Spell;
 
 public class Fire1 extends Spell{
-
+//	public static Fire1 fire1 = new Fire1();
+	public static Fire1 fire1;
+	static {
+		fire1 = new Fire1();
+	}
 	public Fire1() {
 		super(0,0,0,"name");
 		// TODO Auto-generated constructor stub
+	}
+	public static Fire1 get() {
+		return fire1;
 	}
 }
