ff5be22bde064c97aba1731824791f265c25973b
Michael <micron1314@gmail.com>
2015-05-31 22:02:12 -0400

    typos/accident

--- src/Spells/BowSpell.java
+++ src/Spells/BowSpell.java
@@ -13,7 +13,7 @@ public class BowSpell extends Spell{
 	public static String[] name = {"Bow level 0", "Bow level 1", "Bow level 2", "Bow level 3", "Bow level 4", "Bow level 5", "Bow level 6",};
 	public static final String[] filename = {"Bow1.png","Bow2.png","Bow3.png","Bow4.png","Bow5.png","Bow6.png","Bow7.png"};
 	public static Image[] sprite = new Image[7];
-	public static EarthSpell[] spells = {new EarthSpell(0), new EarthSpell(1), new EarthSpell(2), new EarthSpell(3), new EarthSpell(4), new EarthSpell(5), new EarthSpell(6)};
+	public static BowSpell[] spells = {new BowSpell(0), new BowSpell(1), new BowSpell(2), new BowSpell(3), new BowSpell(4), new BowSpell(5), new BowSpell(6)};
 	
 	protected BowSpell(int level){
 //		super(0,0,0,"Bow lv" + level, filenames[level]);
@@ -22,7 +22,7 @@ public class BowSpell extends Spell{
 	}
 	
 	
-	public static EarthSpell get(int n) {
+	public static BowSpell get(int n) {
 		return spells[n];
 	}
 	
