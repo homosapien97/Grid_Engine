8b7e4114fa6087da09f6742a1bf7cb8c833c36d4
1cePrince <herman.sahani1@gmail.com>
2015-06-01 18:45:14 -0700

    Clock and shield completed

--- src/run/Main.java
+++ src/run/Main.java
@@ -16,7 +16,6 @@ public class Main {
 	
 	//player object
 	public static Player player = new Player(0, 0, testChunk, "P", 256, 64, "Player", 0.0, 0.0, 0.0, 0.0, 1, 1);
-	
 	//for debug for now
 	public static final String version = "1.2";
 	
