3f0298818f3304bf7a69e791a0fc8a5d1675199b
1cePrince <herman.sahani1@gmail.com>
2015-06-07 12:01:30 -0700

    Crude map maker
    This is as crude as it gets, use it for now

---
+++ src/tools/Map_maker.java
@@ -0,0 +1,172 @@
+package tools;
+
+
+public class Map_maker 
+{
+	public static void main (String[]args)
+	{
+		final int PARAM1  = 64;
+		final int PARAM2  = 52;
+		final int PARAM3  = 16;
+		final int PARAM4  = 256;
+		final int PARAM5  = 56;
+		final int PARAM6  = 56;
+		final int PARAM7  = 0;
+		final int PARAM8  = 0;
+		final int PARAM9  = 0;
+		final int PARAM10 = 256;
+		final int PARAM11 = 16;
+		
+		for(int a = 0; a<PARAM6; a++)
+		{
+			for(int b = 0; b<PARAM2; b++)
+			{
+				System.out.print("x");
+			}
+			for(int b = 0; b<PARAM3; b++)
+			{
+				System.out.print("o");
+			}
+			for(int b = 0; b<PARAM2; b++)
+			{
+				System.out.print("x");
+			}
+			for(int b = 0; b<PARAM3; b++)
+			{
+				System.out.print("o");
+			}
+			for(int b = 0; b<PARAM2; b++)
+			{
+				System.out.print("x");
+			}
+			for(int b = 0; b<PARAM3; b++)
+			{
+				System.out.print("o");
+			}
+			for(int b = 0; b<PARAM2; b++)
+			{
+				System.out.print("x");
+			}
+			System.out.println();
+		}
+//----------------------------------------------------------------------------------
+		for(int a = 0; a<PARAM11; a++)
+		{
+			for(int b = 0; b<PARAM10; b++)
+			{
+				System.out.print("o");
+			}
+			System.out.println();
+		}
+//----------------------------------------------------------------------------------
+		for(int a = 0; a<PARAM6; a++)
+		{
+			for(int b = 0; b<PARAM2; b++)
+			{
+				System.out.print("x");
+			}
+			for(int b = 0; b<PARAM3; b++)
+			{
+				System.out.print("o");
+			}
+			for(int b = 0; b<PARAM2; b++)
+			{
+				System.out.print("x");
+			}
+			for(int b = 0; b<PARAM3; b++)
+			{
+				System.out.print("o");
+			}
+			for(int b = 0; b<PARAM2; b++)
+			{
+				System.out.print("x");
+			}
+			for(int b = 0; b<PARAM3; b++)
+			{
+				System.out.print("o");
+			}
+			for(int b = 0; b<PARAM2; b++)
+			{
+				System.out.print("x");
+			}
+			System.out.println();	
+		}
+//----------------------------------------------------------------------------------
+		for(int a = 0; a<PARAM6; a++)
+		{
+			for(int b = 0; b<PARAM2; b++)
+			{
+				System.out.print("x");
+			}
+			for(int b = 0; b<PARAM3; b++)
+			{
+				System.out.print("o");
+			}
+			for(int b = 0; b<PARAM2; b++)
+			{
+				System.out.print("x");
+			}
+			for(int b = 0; b<PARAM3; b++)
+			{
+				System.out.print("o");
+			}
+			for(int b = 0; b<PARAM2; b++)
+			{
+				System.out.print("x");
+			}
+			for(int b = 0; b<PARAM3; b++)
+			{
+				System.out.print("o");
+			}
+			for(int b = 0; b<PARAM2; b++)
+			{
+				System.out.print("x");
+			}
+			System.out.println();
+		}
+//----------------------------------------------------------------------------------
+		for(int a = 0; a<PARAM11; a++)
+		{
+			for(int b = 0; b<PARAM10; b++)
+			{
+				System.out.print("o");
+			}
+			System.out.println();
+		}
+//----------------------------------------------------------------------------------
+		for(int a = 0; a<PARAM6; a++)
+		{
+			for(int b = 0; b<PARAM2; b++)
+			{
+				System.out.print("x");
+			}
+			for(int b = 0; b<PARAM3; b++)
+			{
+				System.out.print("o");
+			}
+			for(int b = 0; b<PARAM2; b++)
+			{
+				System.out.print("x");
+			}
+			for(int b = 0; b<PARAM3; b++)
+			{
+				System.out.print("o");
+			}
+			for(int b = 0; b<PARAM2; b++)
+			{
+				System.out.print("x");
+			}
+			for(int b = 0; b<PARAM3; b++)
+			{
+				System.out.print("o");
+			}
+			for(int b = 0; b<PARAM2; b++)
+			{
+				System.out.print("x");
+			}
+			System.out.println();	
+		}
+//----------------------------------------------------------------------------------
+	}
+
+}
